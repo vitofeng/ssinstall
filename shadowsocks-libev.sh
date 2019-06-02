8744d6fa5fdffb7f07bb57c8662f0abb7abca48c
vitofeng <vitofeng@users.noreply.github.com>
2019-06-02 10:38:49 +0800

    Update shadowsocks-libev.sh

--- shadowsocks-libev.sh
+++ shadowsocks-libev.sh
@@ -37,8 +37,8 @@ plain='\033[0m'
 cur_dir=$( pwd )
 software=(Shadowsocks-Python ShadowsocksR Shadowsocks-Go Shadowsocks-libev)
 
-libsodium_file="libsodium-1.0.17"
-libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.17/libsodium-1.0.17.tar.gz"
+libsodium_file="libsodium-1.0.18"
+libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.18/libsodium-1.0.18.tar.gz"
 
 mbedtls_file="mbedtls-2.16.0"
 mbedtls_url="https://tls.mbed.org/download/mbedtls-2.16.0-gpl.tgz"
@@ -69,7 +69,8 @@ shadowsocks_go_debian="https://raw.githubusercontent.com/teddysun/shadowsocks_in
 shadowsocks_libev_init="/etc/init.d/shadowsocks-libev"
 shadowsocks_libev_config="/etc/shadowsocks-libev/config.json"
 shadowsocks_libev_centos="https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev"
-shadowsocks_libev_debian="https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian"
+# shadowsocks_libev_debian="https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian"
+shadowsocks_libev_debian="https://raw.githubusercontent.com/vitofeng/ssinstall/master/shadowsocks-libev-debian"
 
 # Stream Ciphers
 common_ciphers=(
